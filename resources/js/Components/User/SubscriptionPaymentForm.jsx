a6287fd8a122788e9e9e7cef09c3c55c60d66096
Tarique Mosharraf <applandsys@gmail.com>
2025-09-29 02:12:34 +0600

    feat: skk

--- resources/js/Components/User/SubscriptionPaymentForm.jsx
+++ resources/js/Components/User/SubscriptionPaymentForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function SubscriptionPaymentForm() {
+
     const [formData, setFormData] = useState({
         senderId: "",
         comment: "",
